26a75c7c8411a008028f76c1d325570a902a7bb7
thevanayaki <thpr76748@edu.ucl.dk>
2026-05-05 16:14:52 +0200

    jeg færdig at oprette Booking.cs

--- BookRight.Domain/Entities/Booking.cs
+++ BookRight.Domain/Entities/Booking.cs
@@ -9,56 +9,32 @@ namespace BookRight.Domain.Entities
 {
     public class Booking
     {
-        public Guid Id { get; private set; } = Guid.NewGuid();
-
-        public Guid ServiceId { get; set; }
-
-        [ForeignKey("ServiceId")]
-        public Service Service { get; set; }
-        public DateTime Date { get; private set; } // Representerer dagen (fx 2023-12-01)
-                                                   //public TimeSpan Time { get; private set; }  Representerer Klokkeslættet (fx 14:´30:00)
-        public decimal Price { get; private set; }
+        public Guid BookingId { get; private set; } = Guid.NewGuid();
+        public DateTime Date { get; private set; }
+        public DateTime TimeSlot { get; private set; }
+        public int Duration { get; private set; }
+        public decimal price_before_discount { get; private set; }
+        public decimal price_after_discount { get; private set; }
         public BookingStatus Status { get; private set; }
-        // Foreign key til doctor
-        public Guid DoctorId { get; set; }
 
-        // Navigation property: En booking tilhører En læge
-        public Doctor Doctor { get; set; }
-
-        // Andre relationer (kunder og klinik)
+        // Foreign key og Navigation property
         public Guid CustomerId { get; set; }
-
-        [ForeignKey("CustomerId")]
         public Customer Customer { get; set; }
 
-        // Service
+        public List<TreatmentType> CombinedTreatments { get; set; } = new(); // kan undgå null fejl på liste
 
-        public DateTime BookingDate { get; set; }
-
-        public Guid ClinicId { get; set; }
-        public Clinic Clinic { get; set; }
-
-
-
-        public int TreatmentId { get; set; }
-        public Treatment Treatment { get; set; } = default!;
-
-
-        public Guid TreatmentTypeId { get; set; }
-        public TreatmentType TreatmentType { get; set; }
-
-        // en property der returnerer true, hvis status er 'Copleted'
-        public bool IsCompleted => Status == BookingStatus.Completed;
-
-        //EF core skal bruge en constructor uden parameter ( eller en hvor alle kan være null)
         private Booking() { }
-        public Booking(Guid id, DateTime date, decimal price, BookingStatus status)
+        public Booking(Guid Id, DateTime date, DateTime timeSlot, int duration, BookingStatus status)
         {
-            Id = id;
+            BookingId = Id;
             Date = date;
-            Price = price;
+            TimeSlot = timeSlot;
+            Duration = duration;
             Status = status;
         }
+
+
+
     }
 }
-}
+
