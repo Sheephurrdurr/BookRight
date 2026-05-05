8434ec8d92ddc9d179248afc1cfe25e07ec8648b
lenazzy <lenazzy@gmail.com>
2026-05-05 13:11:26 +0200

    Har tiføjet Confirmed, & kommentarer

--- BookRight.Domain/Enums/BookingStatus.cs
+++ BookRight.Domain/Enums/BookingStatus.cs
@@ -6,7 +6,8 @@ namespace BookRight.Domain.Enums
 {
     public enum BookingStatus
     {
-        Completed,
+        Confirmed,//Booking er oprettet/bekræftet
+        Completed,//Kunden er mødt op og behandlingen er gennemført
         Cancelled,
         NoShow
     }
