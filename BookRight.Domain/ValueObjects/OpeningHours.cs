cfbda347617036bdf7e527a30b1480217ef3c5aa
lenazzy <lenazzy@gmail.com>
2026-05-07 12:16:48 +0200

    Slettede OpeningHours.cs fra Domain, og flyttede til ValueObjects.

---
+++ BookRight.Domain/ValueObjects/OpeningHours.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BookRight.Domain.ValueObjects
+{
+    public record OpeningHours
+    {
+        public TimeOnly OpenTime { get; private set; }
+        public TimeOnly CloseTime { get; private set; }
+
+        public OpeningHours(TimeOnly openTime, TimeOnly closeTime)
+        {
+            if (openTime >= closeTime)
+                throw new ArgumentException("Åbningstid skal være før lukketid.");
+
+            OpenTime = openTime;
+            CloseTime = closeTime;
+        }
+    }
+}
