ccd6e7c2b6d6791a24f9afbc7340933a327c9793
Nicolai <nicolai.hoiberg@hotmail.com>
2026-05-07 09:56:04 +0200

    Fixed the PK configs in Customer & therapist configs

--- BookRight.Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
+++ BookRight.Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -8,7 +8,14 @@ namespace BookRight.Infrastructure.Persistence.Configurations
     {
         public void Configure(EntityTypeBuilder<Customer> builder)
         {
-            builder.HasKey(c => c.Id);
+            builder.ToTable("Customers");
+            // PK konfiguration
+            builder.HasKey(c => c.Id); // Sig til EF Core; "Dette er PK"
+            builder.Property(x => x.Id) // Fortæl EF Core hvad vi skal gøre med den PK property
+                .HasConversion( // Oversætter BookingID (fra domain) til Guid (i db) og omvendt.
+                    id => id.Value,
+                    value => new CustomerId(value))
+                .ValueGeneratedNever(); // Db må ikke generere Id. I DDD styrer Domain dette.
 
             builder.OwnsOne(c => c.Name, name =>
             {
