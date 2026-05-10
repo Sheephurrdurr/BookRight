0bb1cdc16d5c6ff9be9337504311b248927326b1
thevanayaki <thpr76748@edu.ucl.dk>
2026-05-10 19:40:08 +0200

    tilføle Address.cs value object

---
+++ BookRight.Domain/ValueObjects/Address.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Domain.ValueObjects
+{
+	public record Address
+	{
+	  public string Street { get; private set; }
+	  public string City { get; private set; }
+	  public string PostalCode { get; private set; }
+	  public string Country { get; private set; }
+
+	  public Address(string street, string city, string postalCode, string country)
+	  {
+			if (string.IsNullOrWhiteSpace(street))
+			    throw new ArgumentException("Street cannot be empty,", nameof(street));
+			if (string.IsNullOrWhiteSpace(city))
+				throw new ArgumentException("City cannot be empty.", nameof(city));
+			if (string.IsNullOrWhiteSpace(PostalCode))
+				throw new ArgumentException("Postalcode cannot be empty.");
+			if (string.IsNullOrWhiteSpace(Country))
+				throw new ArgumentException("Country cannot be empty.", nameof(country));
+
+			Street = street;
+			City = city;
+			PostalCode = postalCode;
+			Country = country;
+		}
+	}
+}
