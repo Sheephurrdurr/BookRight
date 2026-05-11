d227da37f7b528ed0a5f45ffe2fb52800154f6ff
thevanayaki <thpr76748@edu.ucl.dk>
2026-05-11 19:40:54 +0200

    Tilføjer TherapistSchedule

--- BookRight.Domain/Aggregates/Therapist/TherapistSchedule.cs
+++ BookRight.Domain/Aggregates/Therapist/TherapistSchedule.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BookRight.Domain.Aggregates
+namespace BookRight.Domain.Aggregates.Therapist
 {
 	public class TherapistSchedule
 	{
