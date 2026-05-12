93db6ecd91aad570f4a399b98a1e945be5cd6f14
Sofia <sofia_birkeland@hotmail.com>
2026-05-12 10:12:15 +0200

    Lagt ind TherapistTreatmentType entity

---
+++ BookRight.Domain/Aggregates/TreatmentType/TherapistTreatmentType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Domain.Aggregates.TreatmentType
+{
+    public class TherapistTreatmentType
+    {
+        public Guid id { get; private set; }
+        public Guid therapistId { get; private set; }
+        public Guid treatmentTypeId { get; private set; }
+        public decimal basePrice { get; private set; }
+       
+        public TherapistTreatmentType () { }
+
+
+    }
+}
