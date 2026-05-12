9a06732e373d46aa743b987d2102bb638aeb71e4
Sheephurrdurr <nicolai.hoiberg@hotmail.com>
2026-05-12 17:19:03 +0200

    Updated CreateBookingDtos -Added BookingLineRequest dto
    Changed response dto to just return id, for simplicity. Yea, I made that decision

---
+++ BookRight.Facade/DTOs/CreateCustomerDTOs/BookingLineRequest.cs
@@ -0,0 +1,11 @@
+﻿using BookRight.Domain.ValueObjects;
+
+namespace BookRight.Facade.DTOs.CreateCustomerDTOs
+{
+    public record BookingLineRequest
+    {
+        public Guid TherapistTreatmentTypeId { get; set; }
+
+        public Money BasePrice { get; set; }
+    }
+}
