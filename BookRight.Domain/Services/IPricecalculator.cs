aa403d0b7464597734a7e491ac03b9c6d825a46c
philliphermansen-boop <philliphermansen@gmail.com>
2026-05-12 10:24:34 +0200

    pricecalculator og interface i "Services" + Money (VO)

---
+++ BookRight.Domain/Services/IPricecalculator.cs
@@ -0,0 +1,16 @@
+﻿using BookRight.Domain.Aggregates.TreatmentType;
+using BookRight.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Domain.Services
+{
+    public interface IPriceCalculator
+    {
+        Money CalculateBasePrice(TreatmentType treatmenttype);
+        Money ApplyAddOns(Money price, IEnumerable<addon> addOns);
+        DiscountResult ApplyDiscount(Money basePrice, decimal percentage);
+    }
+
+}
---
+++ BookRight.Domain/Services/PriceCalculator.cs
@@ -0,0 +1,33 @@
+﻿using BookRight.Domain.Aggregates.TreatmentType;
+using BookRight.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Domain.Services
+{
+    public class PriceCalculator : IPriceCalculator
+    {
+        public Money CalculateBasePrice(TreatmentType treatmenttype)
+        {
+            return treatmenttype.Price;
+        }
+        
+        public Money ApplyAddOns(Money price, IEnumerable<addon> addOns)
+        {
+            foreach (var addOn in addOns)
+            {
+                price += addOn.price;
+            }
+            return price;
+        }
+        public DiscountResult ApplyDiscount(Money basePrice, decimal percentage)
+        {
+            var discountAmount = basePrice * percentage;
+            var discountedPrice = basePrice - discountAmount;
+
+            return new DiscountResult(basePrice, discountedPrice, discountAmount, percentage);
+
+        }
+    }
+}
