04dec16a97d13bbad39a3d2ea062ad033fad2380
monaroervang <monaroervang@gmail.com>
2026-05-12 14:48:03 +0200

    Adding VO and AddOn to BookingLine.cs

--- BookRight.Domain/Aggregates/AddOn/AddOn.cs
+++ BookRight.Domain/Aggregates/AddOn/AddOn.cs
@@ -1,24 +1,42 @@
 ﻿using BookRight.Domain.ValueObjects;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BookRight.Domain.Aggregates.AddOn
 {
     public class AddOn
     {
         public Guid Id { get; private set; }
-        public string Name { get; private set; }
-        public Money Price { get; private set; }
 
-        private AddOn() { } // parameterløs cunstructor for EF Core
+        public string Name { get; private set; } // Ex. "Weekendtillæg"
 
-        public AddOn(string name, Money price)
+        public decimal Percentage { get; private set; } //Ex. 15 %
+
+        private AddOn() { }
+
+        public AddOn(string name, decimal percentage)
         {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException(
+                    "Name cannot be empty.",
+                    nameof(name));
+
+            if (percentage < 0 || percentage > 100)
+                throw new ArgumentException(
+                    "Percentage must be between 0 and 100.",
+                    nameof(percentage));
+
             Id = Guid.NewGuid();
+
             Name = name;
-            Price = price;
+
+            Percentage = percentage;
+        }
+
+        
+        public Money CalculateAmount(Money basePrice) //Beregner tillæg baseret på pris
+        {
+            decimal multiplier = Percentage / 100;
+
+            return basePrice * multiplier;
         }
     }
-
 }
