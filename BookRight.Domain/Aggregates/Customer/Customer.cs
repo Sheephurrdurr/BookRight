ea5104d3c2ceb274423d3c9f787b46833e40affc
monaroervang <monaroervang@gmail.com>
2026-05-13 10:04:22 +0200

    Customer and Customerconfig. Added tables.

--- BookRight.Domain/Aggregates/Customer/Customer.cs
+++ BookRight.Domain/Aggregates/Customer/Customer.cs
@@ -1,26 +1,29 @@
-﻿using BookRight.Domain.Aggregates.Booking;
-using BookRight.Domain.ValueObjects;
+﻿using BookRight.Domain.ValueObjects;
 
-namespace BookRight.Domain.Aggregates.Customer
+public class Customer
 {
-    public record Customer
+    public Guid Id { get; private set; }
+    public FullName Name { get; private set; }
+    public Email Email { get; private set; }
+    public PhoneNumber Phone { get; private set; }
+    public DateOnly DateOfBirth { get; private set; }
+    public string? HealthNotes { get; private set; } //Nullable
+    public Guid? PreferredTherapistId { get; private set; } //Nullable
+
+    private Customer() { }
+
+    public Customer(FullName name, Email email, PhoneNumber phone, DateOnly dateOfBirth, string? healthNotes, Guid? preferredTherapistId)
     {
-        public Guid Id { get; private set; }
-        public FullName Name { get; private set; }
-        public Email Email { get; private set; }
-        public PhoneNumber Phone { get; private set; }
-        public DateOnly DateOfBirth { get; private set; }
-        public string HealthNotes { get; private set; }
-        public Guid? PreferredTherapistId { get; private set; }
+        Id = Guid.NewGuid();
 
-        private Customer() { } // Kræves af EF Core
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Email = email ?? throw new ArgumentNullException(nameof(email));
+        Phone = phone ?? throw new ArgumentNullException(nameof(phone));
 
-        public Customer(FullName name, Email email, PhoneNumber phone)
-        {
-            Id = Guid.NewGuid();
-            Name = name ?? throw new ArgumentNullException(nameof(name));
-            Email = email ?? throw new ArgumentNullException(nameof(email));
-            Phone = phone ?? throw new ArgumentNullException(nameof(phone));
-        }
+        if (dateOfBirth == default) throw new ArgumentException("Date of birth is required.", nameof(dateOfBirth));
+
+        DateOfBirth = dateOfBirth;
+        HealthNotes = healthNotes;
+        PreferredTherapistId = preferredTherapistId;
     }
 }
