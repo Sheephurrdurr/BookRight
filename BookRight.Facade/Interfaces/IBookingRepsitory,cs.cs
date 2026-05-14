0774d15a62abcffdb6c70b96f48e4eaea1f2910f
thevanayaki <thpr76748@edu.ucl.dk>
2026-05-14 16:33:29 +0200

    Tilføjer IBookingRepository.cs- yaki

---
+++ BookRight.Facade/Interfaces/IBookingRepsitory,cs.cs
@@ -0,0 +1,17 @@
+﻿using BookRight.Domain.Aggregates.Booking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Facade.Interfaces
+{
+	public interface IBookingRepsitory
+	{
+		Task<Booking?> GetByIdAsync(Guid bookingId);
+		Task<IReadOnlyList<Booking>> GetAllAsync(Guid customerId);
+		Task<IReadOnlyList<Booking>> GetByCustomerIdAsync(Guid customerId);
+		Task CreateAsync(Booking booking);
+		Task UpdateAsync(Booking booking);
+		Task Deleteasync(Booking booking);
+	}
+}
