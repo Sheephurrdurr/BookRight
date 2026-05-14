0f1f392b607b4852c92f6ae4974df687e47f5d28
monaroervang <monaroervang@gmail.com>
2026-05-14 13:04:42 +0200

    Fixed warnings.

--- BookRight.Domain/Aggregates/Customer/Customer.cs
+++ BookRight.Domain/Aggregates/Customer/Customer.cs
@@ -6,9 +6,9 @@ namespace BookRight.Domain.Aggregates.Customer;
 public class Customer
 {
     public Guid Id { get; private set; }
-    public FullName Name { get; private set; }
-    public Email Email { get; private set; }
-    public PhoneNumber Phone { get; private set; }
+    public FullName Name { get; private set; } = null!; //Not nullable. It's a promise to the constructor, that Name is set later. Fixes warning. 
+    public Email Email { get; private set; } = null!; //Not nullable
+    public PhoneNumber Phone { get; private set; } = null!; //Not nullable
     public DateOnly DateOfBirth { get; private set; }
     public string? HealthNotes { get; private set; } //Nullable
     public Guid? PreferredTherapistId { get; private set; } //Nullable
