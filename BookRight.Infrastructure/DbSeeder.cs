3515870a5ff0069ff7763cba3982b7cb0de47f28
Sheephurrdurr <nicolai.hoiberg@hotmail.com>
2026-05-15 19:22:05 +0200

    Started creating db seeding class. Need to fix some dependencies first.

---
+++ BookRight.Infrastructure/DbSeeder.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace BookRight.Infrastructure
+{
+    public class DbSeeder
+    {
+        private readonly BookRightDbContext _context;
+
+        public DbSeeder(BookRightDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.SeedAsync();
+        }
+
+        private async Task SeedClinicsAsync()
+        {
+            if (_context.Clinics != null) 
+            {
+                var clinic1 = new Clinic(
+                    "Klinik 1",
+                    new Address("Gade 1", "By", "1234"),
+                    new Domain.ValueObjects     
+            }
+        }
+    }
+}
