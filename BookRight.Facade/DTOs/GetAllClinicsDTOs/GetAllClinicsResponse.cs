0ba176a4b331ac0df86edd718812302441eeeee8
Sheephurrdurr <nicolai.hoiberg@hotmail.com>
2026-05-15 18:37:46 +0200

    Created GetAllClinicsUseCase with interfaces and DI.

---
+++ BookRight.Facade/DTOs/GetAllClinicsDTOs/GetAllClinicsResponse.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Facade.DTOs.GetAllClinicsDTOs
+{
+    public record GetAllClinicsResponse(Guid id, string Name);
+}
