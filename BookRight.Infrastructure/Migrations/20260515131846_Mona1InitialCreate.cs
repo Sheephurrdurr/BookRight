bb82f192d410351de32e624ed33aa76859b8f4c8
monaroervang <monaroervang@gmail.com>
2026-05-15 15:20:09 +0200

    migrations initial create

---
+++ BookRight.Infrastructure/Migrations/20260515131846_Mona1InitialCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookRight.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mona1InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
