4b5ca83a988784728482b54019c49240a354cb0e
Sheephurrdurr <nicolai.hoiberg@hotmail.com>
2026-05-16 22:21:27 +0200

    Created response DTO and Interface for GetAllTherapistTreatmentTypesUseCase

---
+++ BookRight.Facade/DTOs/GetAllTherapistTreatmentTypesDTOs/GetAllTherapistTreatmentTypesResponse.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BookRight.Facade.DTOs.GetAllTherapistTreatmentTypesDTOs
+{
+    public record GetAllTherapistTreatmentTypesResponse(
+        Guid Id,
+        Guid TherapistId,
+        string TherapistName,
+        string TreatmentTypeName,
+        decimal BasePrice
+    );
+}
---
+++ BookRight.Facade/Interfaces/TherapistUseCases/IGetAllTherapistTreatmentTypesUseCase.cs
@@ -0,0 +1,9 @@
+﻿using BookRight.Facade.DTOs.GetAllTherapistTreatmentTypesDTOs;
+
+namespace BookRight.Facade.Interfaces.TherapistUseCases
+{
+    public interface IGetAllTherapistTreatmentTypesUseCase
+    {
+        Task<IReadOnlyList<GetAllTherapistTreatmentTypesResponse>> GetAllTherapistTreatmentTypesAsync();
+    }
+}
