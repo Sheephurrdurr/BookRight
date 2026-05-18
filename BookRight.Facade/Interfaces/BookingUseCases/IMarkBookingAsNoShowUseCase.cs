ad1297118f6010bf0f3dae0c047afe8e59e54901
lenazzy <lenazzy@gmail.com>
2026-05-18 12:35:59 +0200

    Implementeret backend-logik til registrering af No-show bookings.

---
+++ BookRight.Facade/Interfaces/BookingUseCases/IMarkBookingAsNoShowUseCase.cs
@@ -0,0 +1,12 @@
+﻿using BookRight.Facade.DTOs.MarkBookingAsNoShowDTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Facade.Interfaces.BookingUseCases
+{
+    public interface IMarkBookingAsNoShowUseCase
+    {
+        Task ExecuteAsync(MarkBookingAsNoShowRequest request);
+    }
+}
---
+++ BookRight.UseCases/MarkBookingAsNoShow/MarkBookingAsNoShowUseCase.cs
@@ -0,0 +1,30 @@
+﻿using BookRight.Facade.DTOs.MarkBookingAsNoShowDTOs;
+using BookRight.Facade.Interfaces.BookingUseCases;
+using BookRight.UseCases.Interfaces;
+
+namespace BookRight.UseCases.MarkBookingAsNoShow
+{
+    // Use case for registering a NoShow booking.
+    public class MarkBookingAsNoShowUseCase : IMarkBookingAsNoShowUseCase
+    {
+        private readonly IBookingRepository _bookingRepository;
+
+        public MarkBookingAsNoShowUseCase(IBookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+
+        // Executes the use case to mark a booking as a NoShow.
+        public async Task ExecuteAsync(MarkBookingAsNoShowRequest request)
+        {
+            var booking = await _bookingRepository.GetByIdAsync(request.BookingId);
+
+            if (booking is null)
+                throw new KeyNotFoundException("Booking was not found.");
+
+            booking.MarkAsNoShow();
+
+            await _bookingRepository.UpdateAsync(booking);
+        }
+    }
+}
