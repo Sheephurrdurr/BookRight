2eb7c2b625bb068156779842b6c5fb4179160fcd
monaroervang <monaroervang@gmail.com>
2026-05-18 19:44:30 +0200

    Therapist CustomExceptions and Errormessages

--- BookRight.Domain/Errors/DomainErrorMessages.cs
+++ BookRight.Domain/Errors/DomainErrorMessages.cs
@@ -3,28 +3,44 @@
 public static class DomainErrorMessages //Static because we don't want to create an object
 {
     public const string NameCannotBeEmpty = //Const = Known value at run-time and never changes. 
-        "Udfyld navn!";
+        "Navn er påkræ";
 
     public const string AddressCannotBeNull =
-        "Udfyld adresse!";
+        "Adresse er påkrævet";
 
     public const string PhoneNumberCannotBeNull =
-        "Udfyld telefonnummer!";
+        "Telefonnummer er påkrævet";
 
     public const string DateOfBirthIsRequired =
-        "Fødselsdato er påkrævet.";
+        "Fødselsdato er påkrævet";
+
+    public const string SpecializationIsRequired =
+        "Specialisering er påkrævet";
 
     public const string AddOnPercentageOutOfRange =
         "Tillæg skal være mellem 0-100%";
 
     public const string EndDateCannotBeBeforeStartDate =
-        "Slutdato må ikke være før startdato";
+        "Slutdato må ikke være før startdato.";
+
+    public const string DateCannotBeBeforeToday =
+        "Dato må ikke være i fortiden";
 
     public const string NumberOfTreatmentRoomsMustBeGreaterThanZero =
         "Antal behandlingsrum skal være større end 0";
 
     public const string OpeningTimeMustBeBeforeClosingTime =
         "Åbningstid skal være før lukketid";
+
+    public const string TherapistIsNotAvailable =
+        "Behandleren er ikke tilgængelig på det valgte tidspunkt";
+
+    public const string TherapistAlreadyHasBooking =
+        "Behandleren er allerede booket på det valgte tidspunkt";
+
+    public const string PriceMustBeGreaterThanZero =
+        "Prisen skal være højere end 0 kr.";
+
 }
     
 
---
+++ BookRight.Domain/Exceptions/TherapistIsNotAvailableException.cs
@@ -0,0 +1,12 @@
+﻿
+using BookRight.Domain.Errors;
+
+namespace BookRight.Domain.Exceptions;
+
+public class TherapistIsNotAvailableException : DomainException
+{
+    public TherapistIsNotAvailableException()
+        : base(DomainErrorMessages.TherapistIsNotAvailable)
+    {
+    }
+}
