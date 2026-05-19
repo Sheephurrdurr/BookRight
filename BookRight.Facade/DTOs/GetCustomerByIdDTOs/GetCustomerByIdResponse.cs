f2d10377929c8e715bd6f295c3fedde9c2cbfe24
Nicolai <nicolai.hoiberg@hotmail.com>
2026-05-19 11:26:11 +0200

    Created DTO's for GetCustomerById use case.
    Started GetCustomerByIdUseCase

---
+++ BookRight.Facade/DTOs/GetCustomerByIdDTOs/GetCustomerByIdResponse.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BookRight.Facade.DTOs.GetCustomerByIdDTOs
+{
+    public record GetCustomerByIdResponse(
+        Guid Id,
+        string FirstName,
+        string LastName,
+        string Email,
+        string Phone,
+        DateOnly DateOfBirth,
+        Guid? PreferredTherapistId
+    );
+
+    // DTO returneres kun af use cases der har brug for den, så følsom brugerdata ikke bliver exponeret.
+    public record CustomerHealthNoteResponse(
+        Guid CustomerId,
+        string? Healthnote
+    );
+}
---
+++ BookRight.UseCases/GetCustomerById/GetCustomerByIdUseCase.cs
@@ -0,0 +1,30 @@
+﻿using BookRight.Facade.DTOs.GetCustomerByIdDTOs;
+using BookRight.UseCases.Interfaces;
+
+namespace BookRight.UseCases.GetCustomerById
+{
+    public class GetCustomerByIdUseCase
+    {
+        private readonly ICustomerRepository _repository;
+
+        public GetCustomerByIdUseCase(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetCustomerByIdResponse> ExecuteAsync(Guid customerId)
+        {
+            var customers = await _repository.GetByIdAsync(customerId);
+
+            return customers.Select(c => new GetCustomerByIdResponse(
+                c.Id,
+                c.Name.FirstName,
+                c.Name.LastName,
+                c.Email.Value,
+                c.Phone.Value,
+                c.Birth
+                
+            )).ToList();
+        }
+    }
+}
