45c3ea62f36bfebd2add8a9d1666cb71c4175e00
philliphermansen-boop <philliphermansen@gmail.com>
2026-05-19 16:35:58 +0200

    GetAvailableTherapist + DTO -- VIGTIG!! Har tilføjet denne linje i clinicrepository: .Include(c => c.TherapistSchedules) Og disse i Clinic.cs: private readonly List<TherapistSchedule> _therapistSchedules = new(); public IReadOnlyCollection<TherapistSchedule> TherapistSchedules => _therapistSchedules.AsReadOnly();
    ved ikke om man skal lave en ny migration?

---
+++ BookRight.Facade/DTOs/GetAvailableTherapistsDTOs/GetAvailableTherapistsRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Facade.DTOs.GetAvailableTherapistsDTOs
+{
+    public class GetAvailableTherapistsRequest
+    {
+        public Guid ClinicId { get; set; }
+        public DateOnly Date { get; set; }
+    }
+}
---
+++ BookRight.Facade/Interfaces/TherapistUseCases/IGetAvailableTherapistsUseCase.cs
@@ -0,0 +1,12 @@
+﻿using BookRight.Facade.DTOs.GetAvailableTherapistsDTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Facade.Interfaces.TherapistUseCases
+{
+    public interface IGetAvailableTherapistsUseCase
+    {
+        Task<List<AvailableTherapistResponse>> ExecuteAsync(GetAvailableTherapistsRequest request);
+    }
+}
