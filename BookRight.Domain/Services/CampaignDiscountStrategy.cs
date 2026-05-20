8bd1cb63d681d18167a2a6fca7e2a47587b52fa5
Sofia <sofia_birkeland@hotmail.com>
2026-05-20 07:17:45 +0200

    CampaignDiscountStrategy created, maybe

---
+++ BookRight.Domain/Services/CampaignDiscountStrategy.cs
@@ -0,0 +1,28 @@
+﻿using BookRight.Domain.Aggregates.Booking;
+using BookRight.Domain.Aggregates.Customer;
+using BookRight.Domain.Enums;
+using BookRight.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Domain.Services
+{
+    public class CampaignDiscountStrategy //Interface når den er lagt ind
+    {
+        private readonly decimal _discountPercent; //Private field
+
+        public CampaignDiscountStrategy (decimal discountPercent)
+        {
+            _discountPercent = discountPercent;
+        }
+
+        public DiscountResult CalculateDiscount(Customer customer, Booking booking, IEnumerable<Booking> completedBookings) //lidt usikker med parameter her...
+        {
+            var originalPrice = booking.GetTotalPrice(); //Recieve base price of booking
+            return new DiscountResult(originalPrice, originalPrice * (1m - _discountPercent), DiscountType.Campaign.ToString());
+        }
+
+                                                   
+    }
+}
