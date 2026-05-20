6148c4ddea5a878b3ed30df2099e2261f10b8833
Sofia <sofia_birkeland@hotmail.com>
2026-05-20 21:50:11 +0200

    Lavet en "BasePrice" metode

--- BookRight.Domain/Services/CampaignDiscountStrategy.cs
+++ BookRight.Domain/Services/CampaignDiscountStrategy.cs
@@ -19,7 +19,7 @@ namespace BookRight.Domain.Services
 
         public DiscountResult CalculateDiscount(Customer customer, Booking booking, IEnumerable<Booking> completedBookings)
         {
-            var originalPrice = booking.GetTotalPrice(); //Recieve base price of booking
+            var originalPrice = booking.GetBasePrice(); //Recieve base price of booking
             return new DiscountResult(originalPrice, originalPrice * (1m - (_discountPercent/100)), DiscountType.Campaign.ToString());
             //base price * 1 - % men i decimal, 15% = 0.15, dermed Returnerer 85% af originalprisen ved 15% rabat som nypris,
                                                        //_discountPercent/100 fordi receptionist ikke skal taste 0,15, men 15%
