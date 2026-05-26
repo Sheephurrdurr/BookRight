af898c1af62862d7d492dc46e68f0f7b95bf7174
Sofia <sofia_birkeland@hotmail.com>
2026-05-26 09:50:44 +0200

    WIP

--- BookRight.Domain.Test/TreatmentTypeTest.cs
+++ BookRight.Domain.Test/TreatmentTypeTest.cs
@@ -1,19 +1,14 @@
 ﻿using BookRight.Domain.Aggregates.TreatmentType;
-using BookRight.Domain.Exceptions;
 using BookRight.Domain.ValueObjects;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit.Sdk;
 
 namespace BookRight.Domain.Test
 {
     public class TreatmentTypeTest
     {
-        //Treatmenttype oprettelse uden navn giver domain exception
+        //Treatmenttype oprettelse uden navn giver exception
         [Fact]
         
-        public void CreateTreatmentType_NoName_DomainException()
+        public void CreateTreatmentType_NoName_Exception()
         {
             //Arrange
             var name = "";
@@ -25,12 +20,103 @@ namespace BookRight.Domain.Test
 
             //Act & Assert
             Assert.Throws<ArgumentException>(() =>
-                new TreatmentType(name, durationMin, maxParticipants, money, combination, requiredSpecialization));
+                new TreatmentType(
+                    name, 
+                    durationMin,
+                    maxParticipants,
+                    money, 
+                    combination,
+                    requiredSpecialization));
+        }
+        //TreatmentType oprettelse med duration på under 1 giver exception
+        [Fact]
+        public void CreateTreatmentType_LessThan1min_Exception()
+        {
+            //Arrange
+            var name = "Name";
+            int durationMin = 0;
+            int maxParticipants = 1;
+            var money = new Money(400);
+            bool combination = true;
+            var requiredSpecialization = "Massør";
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() =>
+                new TreatmentType(
+                    name, 
+                    durationMin,
+                    maxParticipants,
+                    money, 
+                    combination,
+                    requiredSpecialization));
         }
 
+        //TreamtnType giver exception om under 1 deltager
+        [Fact]
+        public void CreateTreatmentType_LessThan1Participant_Exception()
+        {
+            //Arrange
+            var name = "Name";
+            int durationMin = 40;
+            int maxParticipants = -4;
+            var money = new Money(400);
+            bool combination = true;
+            var requiredSpecialization = "Massør";
 
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() =>
+                new TreatmentType(
+                    name,
+                    durationMin,
+                    maxParticipants,
+                    money,
+                    combination, 
+                    requiredSpecialization));
+        }
 
+        //TreatmentType ændringer giver faktisk ændring
+        [Fact]
+        public void EditTreatmentTypeName_ChangesName()
+        {
+            //Arrange
+            var name = "Name";
+            int durationMin = 45;
+            int maxParticipants = 1;
+            var money = new Money(400);
+            bool combination = true;
+            var requiredSpecialization = "Massør";
 
+            var testTreatmentType = new TreatmentType(
+                name, 
+                durationMin,
+                maxParticipants,
+                money, 
+                combination, 
+                requiredSpecialization);
+            //Act
+            var newName = "NewName";
+            int newDurationMin = 45;
+            int newMaxParticipants = 1;
+            var newMoney = new Money(400);
+            bool newCombination = true;
+            var newRequiredSpecialization = "Massør";
+
+            testTreatmentType.UpdateTreatmentType(
+                newName,
+                newDurationMin,
+                newMaxParticipants,
+                newMoney,
+                newCombination, 
+                newRequiredSpecialization);
+            //Assert
+            Assert.Equal(newName, testTreatmentType.Name);
+        }
 
     }
+
+
+
+
+
+    
 }
