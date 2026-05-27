9b06aa9d5c3005bb82508a3e9ca811f9af0a1296
lenazzy <lenazzy@gmail.com>
2026-05-27 18:31:46 +0200

    Add update clinic feature er done

---
+++ BookRight.Facade/DTOs/UpdateClinicDTOs/UpdateClinicRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace BookRight.Facade.DTOs.UpdateClinicDTOs
+{
+    public record UpdateClinicRequest(
+        Guid ClinicId,
+        string Name,
+        string Street,
+        string City,
+        string PostalCode,
+        string Phone,
+        int NumTreatmentRooms);
+}
