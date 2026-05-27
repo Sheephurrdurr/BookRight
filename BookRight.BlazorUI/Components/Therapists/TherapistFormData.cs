ce25dfe250b0bd8c8596075fc313b3271b0af15c
Sheephurrdurr <nicolai.hoiberg@hotmail.com>
2026-05-27 22:51:04 +0200

    I think that was all of therapist isolated into components... And with a Blazor layer DTO for editing. Might've forgotten something... I dont know anymore. Now we just need aaaaall the rest. But first - sleep!

---
+++ BookRight.BlazorUI/Components/Therapists/TherapistFormData.cs
@@ -0,0 +1,15 @@
+﻿namespace BookRight.BlazorUI.Components.Therapists
+{
+    // positional record here, fight me. I'm not doing object initializer syntax for all that. Idc I hate it.
+    public record TherapistFormData(
+        string FirstName,
+        string LastName,
+        string Email,
+        string Specialization,
+        string AuthorizationType,
+        string AuthorizationNumber,
+        Guid ClinicId,
+        List<Guid> TreatmentTypeIds
+        );
+    
+}
